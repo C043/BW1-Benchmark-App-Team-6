f944871e567131f47218b752c7481537d11b7b40
MelRecalde <melquisedicrecalde@gmail.com>
2024-05-20 18:08:26 +0200

    commit 6pm

---
+++ assets/script/index.js
@@ -0,0 +1,23 @@
+/*
+const checkBox = document.getElementsByClassName("inp")
+
+checkBox.addEventListener("click", checkBox() => {
+    checkBox.classList.add("checked")
+})
+checkBox()
+
+
+const button = document.getElementById("btn")
+
+button.addEventListener("click", () => {
+    if () {
+        const url = "./question.html"
+        window.location.href = url
+    } else {
+
+    }
+}
+)*/
+
+// 1. checkbox required prima di premere il proceed
+// 2. "if" : è stato cliccato il checkBox 
