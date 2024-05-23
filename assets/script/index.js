64f2c8c7c59523f359aa9742348555d04900635e
MelRecalde <melquisedicrecalde@gmail.com>
2024-05-23 13:04:11 +0200

    button fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,15 +6,23 @@ checkBox.addEventListener("click", () => {
         const label1 = document.getElementById("lbl") // questa è la label non cliccato
         label1.classList.add("checked") // creo una nuova classe alla label 
         label1.append(checkBox) // appendo l'input nella label come "fratello"
+        const bottone = document.querySelector(".proceed")
+        bottone.classList.remove("proceed")
+        bottone.classList.add("goTo")
     } else {
-        const label2 = document.querySelector("#lbl") // tag input
+        const label2 = document.getElementById("lbl") // tag input
         label2.classList.remove("checked") // rimuovo la classe quando è cliccato
         label2.classList.add("labelForInput") // rimetto la classe iniziale(vuoto)
         label2.append(checkBox) // appendo l'input nella label come "fratello"
+        const bottone1 = document.querySelector(".goTo")
+        bottone1.classList.remove("goTo")
+        bottone1.classList.add("proceed")
+        
+        
     }
 }
 )
-const button = document.getElementById("btn") // è il bottone
+const button = document.getElementById("btn") // è lo span
 // se il checkbox è checked va nella prossima pagina(question) / in caso contrario mi dice di fare il check sull'input
 button.addEventListener("click", () => {
     if (checkBox.checked === true) {
